d638348263843f55ec60a6ace3ac2a4427c373d7
Andrew <arhorn@smcm.edu>
2019-01-24 02:15:33 +0700

    ran prettier

--- src/pages/my-files.js
+++ src/pages/my-files.js
@@ -7,27 +7,27 @@ export default ({ data }) => {
   return (
     <Layout>
       <div>
-      <h1>My Site's Files</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>relativePath</th>
-            <th>prettySize</th>
-            <th>extensions</th>
-            <th>birthTime</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.allFile.edges.map(({ node }, index) => (
-            <tr key={index}>
-              <td>{node.relativePath}</td>
-              <td>{node.prettySize}</td>
-              <td>{node.extensions}</td>
-              <td>{node.birthTime}</td>
+        <h1>My Site's Files</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>relativePath</th>
+              <th>prettySize</th>
+              <th>extensions</th>
+              <th>birthTime</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {data.allFile.edges.map(({ node }, index) => (
+              <tr key={index}>
+                <td>{node.relativePath}</td>
+                <td>{node.prettySize}</td>
+                <td>{node.extensions}</td>
+                <td>{node.birthTime}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </Layout>
   )
